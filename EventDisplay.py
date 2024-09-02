Add masking out of channels, fix plotting of time
Also add option to label mPMTs with a number

EventDisplay.py
@@ -16,6 +16,10 @@ class EventDisplay:
         self.nmPMTs = len(self.mPMT_2D_projection[:,0])
 
         self.nChannels = 19*self.nmPMTs
+        self.mask_list = None
+
+    def mask_mPMTs(self,mask_list):
+        self.mask_list = mask_list
 
     def process_data(self,pmts, pmt_data,sum_data=True):
         #takes as input a list of pmts and pmt_data
@@ -30,9 +34,9 @@ class EventDisplay:
             else:
                 #chose the smallest value of data for that channel e.g time  
                 if(outData[iPMT]>0):
-                    outData[iPMT]=min(outData[iPMT],pmt_data)
+                    outData[iPMT]=min(outData[iPMT],channel_data)
                 else:
-                    outData[iPMT]=pmt_data
+                    outData[iPMT]=channel_data
                  
         return outData
 
@@ -87,9 +91,14 @@ class EventDisplay:
         axis_ranges = np.ptp(coordinates, axis=0)
         figsize = (20*scale, 16*scale*axis_ranges[1]/axis_ranges[0])
 
-        #set the style for plotting
         if not show_zero:
             data[data == 0] = np.nan
+            
+        if self.mask_list is not None:
+            for mPMT_mask in self.mask_list:
+                data[np.arange(self.nChannels)//19 == mPMT_mask] = np.nan    
+            
+        #set the style for plotting
         color_map = copy.copy(color_map)
         if style == "dark_background":
             edge_color = '0.35'
@@ -106,6 +115,7 @@ class EventDisplay:
         fig_width = matplotlib.rcParams['figure.figsize'][0]
         scale = fig_width/20
         
+        
         #draw circles around each mPMT
         pmt_circles = [Circle((pos[0], pos[1]), radius=0.48) for pos in self.mPMT_2D_projection[:,1:3]]
         ax.add_collection(PatchCollection(pmt_circles, facecolor='none', linewidths=1*scale, edgecolors=edge_color))
@@ -114,3 +124,18 @@ class EventDisplay:
         pmts = ax.scatter(coordinates[:, 0], coordinates[:, 1], c=data, s=7*scale*scale, cmap=color_map, norm=color_norm)
         fig.colorbar(pmts, ax=ax, pad=0, label=color_label)
     
+    def label_mPMT_plot(self, mPMT_label):
+        
+        coordinates_x = self.mPMT_2D_projection[:,1] 
+        coordinates_y = self.mPMT_2D_projection[:,2]
+        
+        if(len(mPMT_label)!= self.nmPMTs):
+            raise Exception ("label_mPMT_plot designed to have a label for each mPMT")
+        
+        for (x, y, label) in zip(coordinates_x,coordinates_y, mPMT_label):
+            if(label is not None):
+                plt.text(x, y, str(label), fontsize=10, ha='center', va='center')
+
+
+        
+        

main.py
@@ -13,11 +13,18 @@ eventDisplay = EventDisplay()
 # eventDisplay.load_mPMT_positions('mPMT_locations_WCSim_container_centreX.csv')
 #event display with z axis coming out of page
 eventDisplay.load_mPMT_positions('mPMT_locations_WCSim_container_centreZ.csv')
+# eventDisplay.load_mPMT_positions('mPMT_2D_projection_angles.csv')
+
+#mask out mPMT slots - nb this will only affect the event display the data loading is unchanged
+#WCTE slot numbering
+# eventDisplay.mask_mPMTs([45,77,79,27,32,85,91,99,12,14,16,18])
+#WCSim container numbering
+eventDisplay.mask_mPMTs([20,73,38,49,55,65,67,33,71,92,101,95])
 
 # #3) load the data to plot
 # plot some simulated event
 eventID =0
-data = np.load("wcsim_pi+_Beam_600MeV_30cm_0000.npz", allow_pickle=True)
+data = np.load("test_data/wcsim_pi+_Beam_600MeV_30cm_0000.npz", allow_pickle=True)
 pmt_id = data["digi_hit_pmt"][eventID]
 data_to_plot = data["digi_hit_charge"][eventID]
 #process data has a sum_data option set to true by default, 
@@ -31,7 +38,8 @@ plt.close()
 
 # #3) Or plot geometry data to check the event display mapping is orientated correctly
 # #plot the geometry information to check that the event display is orientated correctly 
-data = np.load("wcsim_pi+_Beam_400MeV_30cm_0000geo.npz", allow_pickle=True)
+
+data = np.load("test_data/wcsim_pi+_Beam_400MeV_30cm_0000geo.npz", allow_pickle=True)
 pmt_id = data["tube_no"]-1
 
 for i, axis in enumerate(["x","y","z"]):
@@ -41,4 +49,3 @@ for i, axis in enumerate(["x","y","z"]):
     plt.savefig("PMT_"+axis+"_position.png")
     input()
     plt.close()
-